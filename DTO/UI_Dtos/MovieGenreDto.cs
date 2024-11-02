2804ca8b06f86fc71a11e362791ff042c5f2c753
İbrahim Kaymak <ibrahimkaymak817@outlook.com>
2024-11-02 16:59:13 +0300

    default/index ,, viewcomponents

---
+++ DTO/UI_Dtos/MovieGenreDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.UI_Dtos
+{
+    public class MovieGenreDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Image2 { get; set; }
+        public string Description { get; set; }
+        public string Description2 { get; set; }
+        public string Director { get; set; }
+        public string Writer { get; set; }
+        public string Trailer { get; set; }
+        public string HashTag { get; set; }
+
+        public int Score { get; set; }
+        public int Year { get; set; }
+        public int RunTime { get; set; }
+        public int ImbdScore { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public List<string> Genres { get; set; } // Tür isimlerini içeren liste
+    }
+}
---
+++ MovieProject.UI/ViewComponents/_SliderVC.cs
@@ -0,0 +1,31 @@
+﻿using DTO.UI_Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace MovieProject.UI.ViewComponents
+{
+    public class _SliderVC : ViewComponent
+    {
+        private readonly IHttpClientFactory _client;
+
+        public _SliderVC(IHttpClientFactory client)
+        {
+            _client = client;
+        }
+
+        [HttpGet]
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var clientx = _client.CreateClient();
+            var responsemessage = await clientx.GetAsync("https://localhost:44358/api/MovieGenre");
+            if (responsemessage.IsSuccessStatusCode)
+            {
+                var jsondata = await responsemessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<MovieGenreDto>>(jsondata);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
